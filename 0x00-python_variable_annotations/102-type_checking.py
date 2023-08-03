4ef55749fd2845192d48bdbf9813ade86ce05014
Akanni Emmanuel <akannie33@gmail.com>
2023-08-03 16:54:57 -0700

    🩹🔧 I promised I've fixed it this time

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,18 +5,18 @@ type checking
 import typing
 
 
-def zoom_array(lst: typing.List, factor: int = 2) -> typing.List:
+def zoom_array(lst: typing.Tuple, factor: int = 2) -> typing.List:
     """
     zoom array
     """
     zoomed_in: typing.List = [
         item for item in lst
-        for i in range(factor)
+        for i in range(int(factor))
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
