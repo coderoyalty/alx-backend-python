1ba27c97002490e35a546f8ddece30ce04de8e77
Akanni Emmanuel <akannie33@gmail.com>
2023-08-03 06:40:12 -0700

    ✅ task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+2-floor: 2. Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    return the floor of `n` as an integral.
+    """
+    return int(n)
