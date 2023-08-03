b82a338eeb6cc87b03e31b36413ab99290d72510
Akanni Emmanuel <akannie33@gmail.com>
2023-08-03 06:33:33 -0700

    ✅ task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+0-add: 0. Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    function with type annotation, that calculates the sum
+    of `a` and `b`
+    """
+    return (a + b)
