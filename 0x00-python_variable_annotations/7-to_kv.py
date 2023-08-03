6b722aab02dc635fff9595038517f91bea2022c8
Akanni Emmanuel <akannie33@gmail.com>
2023-08-03 07:01:10 -0700

    ✅ task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+7-to_kv
+"""
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """
+    return `k` and `v` as a tuple
+    """
+    return (k, v)
