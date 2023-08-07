5876d4040fd5c5215e5797a3f6a1c8bf6ba63066
Akanni Emmanuel <akannie33@gmail.com>
2023-08-07 11:53:46 -0700

    ✅ 0x01: task 2 completed

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+2-measure_runtime
+"""
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    measure time taken to run the `wait_n` function
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+
+    return (end-start) / n
