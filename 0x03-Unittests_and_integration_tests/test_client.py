1e4ccac8f79e78ac78e13ca97ab6715d44c67a82
Akanni Emmanuel <akannie33@gmail.com>
2023-09-02 00:45:42 +0100

    0x03 task 5: mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -23,3 +23,14 @@ class TestGithubOrgClient(unittest.TestCase):
         ORG_URL = GithubOrgClient.ORG_URL
         client.org()
         mock.assert_called_once_with(ORG_URL.format(org=org))
+
+    def test_public_repos_url(self):
+        """ unittest _public_repos_url """
+        with unittest.mock.patch(
+            "client.GithubOrgClient.org",
+                new_callable=unittest.mock.PropertyMock) as mock:
+            client = GithubOrgClient('apple')
+            payload = {"repos_url": "meta"}
+            mock.return_value = payload
+            res = client._public_repos_url
+            self.assertEqual(res, payload["repos_url"])
