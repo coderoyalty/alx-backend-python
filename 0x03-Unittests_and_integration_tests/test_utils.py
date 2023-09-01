283881ea05495ec10e2da408d8566d38a545db0c
Akanni Emmanuel <akannie33@gmail.com>
2023-09-01 23:33:11 +0100

    0x03: unittest for get_json, mocked the 'get' method of requests module

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,10 @@
 unittest for utils.py
 """
 import unittest
+import unittest.mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,3 +30,23 @@ class TestAccessNestedMap(unittest.TestCase):
         """test access_nested_map for exception"""
         with self.assertRaises(KeyError) as err:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    unittest class for utils.get_json
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ test utils.get_json """
+
+        with unittest.mock.patch('requests.get') as mock:
+            mock.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(test_url), test_payload)
+
+            # assert the mock was called once with the URL
+            mock.assert_called_once_with(test_url)
